4a6ca2ec638d0a9178cab415211193952e23a248
Conner Ardman <ardmanc@cs.washington.edu>
2018-11-26 12:22:17 -0800

    Updates

--- precache-manifest.d0f0ac7c8bcad47c7a1bc3d5e91d240a.js
+++ precache-manifest.d0f0ac7c8bcad47c7a1bc3d5e91d240a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/stage-0-topic-proposal-ConnerArdman/static/js/runtime~main.fb81e267.js"
   },
   {
-    "revision": "804fd5b725f0ead496f6",
-    "url": "/stage-0-topic-proposal-ConnerArdman/static/js/main.804fd5b7.chunk.js"
+    "revision": "f4af5140f04697a5eb44",
+    "url": "/stage-0-topic-proposal-ConnerArdman/static/js/main.f4af5140.chunk.js"
   },
   {
     "revision": "9ad1edfcb85d0583b085",
     "url": "/stage-0-topic-proposal-ConnerArdman/static/js/1.9ad1edfc.chunk.js"
   },
   {
-    "revision": "804fd5b725f0ead496f6",
-    "url": "/stage-0-topic-proposal-ConnerArdman/static/css/main.cb2678e3.chunk.css"
+    "revision": "f4af5140f04697a5eb44",
+    "url": "/stage-0-topic-proposal-ConnerArdman/static/css/main.8b25346d.chunk.css"
   },
   {
-    "revision": "950f506b386d9307a7a14b06cb4a8f5a",
+    "revision": "00f069b74ad10cabfde40c80d43a39d5",
     "url": "/stage-0-topic-proposal-ConnerArdman/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stage-0-topic-proposal-ConnerArdman/precache-manifest.0362c225dc5144f7e45b8eeded291446.js"
+  "/stage-0-topic-proposal-ConnerArdman/precache-manifest.d0f0ac7c8bcad47c7a1bc3d5e91d240a.js"
 );
 
 workbox.clientsClaim();
