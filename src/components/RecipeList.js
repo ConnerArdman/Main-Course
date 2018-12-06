0d32ce74d14baaf4243488cf823b8c7fb9ae265b
Conner Ardman <ardmanc@cs.washington.edu>
2018-12-06 13:05:46 -0800

    Cleanup code

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,7 +6,7 @@ import ReactLoading from 'react-loading';
 export class RecipeList extends Component {
    render() {
       const {hits, deleteRecipe, loading} = this.props;
-      const loaded = hits.length !== 0
+      const loaded = hits.length !== 0;
 
       return (
          <section id="recipes">
@@ -14,28 +14,28 @@ export class RecipeList extends Component {
 
             <div id="plan-container" className={!loaded ? "hidden" : null}>
                   {loading ?
-                        <div>
-                              <ReactLoading id="loading" type={"spin"} color={"Black"} height={150} width={150}/>
-                              <div class="push"></div><div class="push"></div>
-                        </div>
-                        :
-                        <div>
-                        <ShoppingList hits={hits} />
-                        {hits.map((hit, index) => {
-                              return <Recipe
-                              hit={hit}
-                              key={index}
-                              deleteRecipe={() => deleteRecipe(index)} />
-                        })}
-                        </div>
-                        }
+                     <div>
+                        <ReactLoading id="loading" type={"spin"} color={"Black"} height={150} width={150}/>
+                        <div class="push"></div><div class="push"></div>
+                     </div>
+                     :
+                     <div>
+                     <ShoppingList hits={hits} />
+                     {hits.map((hit, index) => {
+                           return <Recipe
+                           hit={hit}
+                           key={index}
+                           deleteRecipe={() => deleteRecipe(index)} />
+                     })}
+                     </div>
+                  }
             </div>
 
             <p id="plan-placeholder" className={loaded ? "hidden" : null}>
-                  {loading ?
-                        <ReactLoading id="loading" type={"spin"} color={"Black"} height={150} width={150} /> :
-                        <p>Meal plan will appear here once it is created!</p>}
-                  <div class="push"></div><div class="push"></div>
+               {loading ?
+                  <ReactLoading id="loading" type={"spin"} color={"Black"} height={150} width={150} /> :
+                  <p>Meal plan will appear here once it is created!</p>}
+               <div class="push"></div><div class="push"></div>
             </p>
          </section>
       );
